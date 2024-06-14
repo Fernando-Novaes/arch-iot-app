b095ba4554b8054a6cf79e3a195bb694e30a05d2
Fernando <fernandonrs@cos.ufrj.br>
2024-06-13 22:25:56 -0300

    Changed to use database H2.

--- src/main/java/br/ufrj/cos/components/treeview/TreeViewComponent.java
+++ src/main/java/br/ufrj/cos/components/treeview/TreeViewComponent.java
@@ -5,12 +5,15 @@ import br.ufrj.cos.components.notification.NotificationDialog;
 import br.ufrj.cos.domain.*;
 import br.ufrj.cos.service.IoTDomainService;
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.grid.GridVariant;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.treegrid.TreeGrid;
 import com.vaadin.flow.data.renderer.ComponentRenderer;
+import com.vaadin.flow.spring.annotation.SpringComponent;
+import com.vaadin.flow.spring.annotation.UIScope;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -19,7 +22,8 @@ import java.util.*;
 
 import static javax.swing.text.StyleConstants.setIcon;
 
-@Component
+@SpringComponent
+@UIScope
 @CssImport(value = "./styles/app-styles.css", themeFor = "vaadin-grid")
 public class TreeViewComponent extends VerticalLayout {
 
@@ -31,59 +35,60 @@ public class TreeViewComponent extends VerticalLayout {
     }
 
     @PostConstruct
-    public void init() {
-        TreeGrid<Object> treeGrid = new TreeGrid<>();
-        List<IoTDomain> rootNodes = ioTDomainService.findAllDomains();
+    private void init() {
+        // Fetch data and build tree
+        TreeNode<IoTDomain> rootNode = ioTDomainService.buildIoTDomainsTree();
 
-        treeGrid.addHierarchyColumn(this::getNodeName).setHeader("Name");
-        treeGrid.setItems(ioTDomainService.getChildren(rootNodes));
+        // Create a TreeGrid to display the hierarchical data
+        TreeGrid<TreeNode<IoTDomain>> treeGrid = new TreeGrid<>();
+        treeGrid.setItems(rootNode.getChildren(), TreeNode::getChildren);
 
-        treeGrid.setHierarchyColumn(new ComponentRenderer<>(item -> {
-            Span nameSpan = new Span(getNodeName(item));
-            Image icon = new Image();
-            setIcon(icon, item);
-
-            icon.setClassName("tree-icon");
-            Div container = new Div(icon, nameSpan);
-            container.setClassName("tree-item-container");
-            return container;
-        }).toString());
-
-        treeGrid.addItemClickListener(event -> {
-            Object item = event.getItem();
-            if (item instanceof Technology technology) {
-                List<String> path = ioTDomainService.getPath(technology);
-                NotificationDialog
-                        .showNotificationDialogOnBotton(technology.getDescription()+":" ,
-                                 String.join(" -> ", path));
+        // Define columns (e.g., displaying IoT Domain names)
+        treeGrid.addComponentColumn(item -> {
+            if (item.getData() != null) {
+                return new Span(item.getData().getName());
             }
+            return null;
+        }).setHeader("IoT Domains");
+
+        // Expand all rows by default
+        treeGrid.expandRecursively(rootNode.getChildren(), 2);
+
+        // Add a click listener to the TreeGrid nodes
+        treeGrid.addItemClickListener(event -> {
+            TreeNode<IoTDomain> node = event.getItem();
+            List<TreeNode<IoTDomain>> path = getPathToRoot(node);
+
+            // Construct the full path string
+            StringBuilder pathString = new StringBuilder();
+            for (int i = path.size() - 1; i >= 0; i--) {
+                pathString.append(path.get(i).getData().getName());
+                if (i > 0) {
+                    pathString.append(" > ");
+                }
+            }
+
+            // Print or display the full path
+            System.out.println("Full path of clicked node: " + pathString.toString());
         });
 
+        // Add styling variant to the TreeGrid for better visibility
+        treeGrid.addThemeVariants(GridVariant.LUMO_ROW_STRIPES);
+
+        // Add the TreeGrid to the main layout
         add(treeGrid);
     }
 
-    private String getNodeName(Object node) {
-        if (node instanceof IoTDomain) {
-            return ((IoTDomain) node).getName();
-        } else if (node instanceof ArchitectureSolution) {
-            return ((ArchitectureSolution) node).getName();
-        } else if (node instanceof QualityRequirement) {
-            return ((QualityRequirement) node).getName();
-        } else if (node instanceof Technology) {
-            return ((Technology) node).getDescription();
-        }
-        return "";
-    }
+    private List<TreeNode<IoTDomain>> getPathToRoot(TreeNode<IoTDomain> node) {
+        List<TreeNode<IoTDomain>> path = new ArrayList<>();
+        TreeNode<IoTDomain> current = node;
 
-    private void setIcon(Image icon, Object item) {
-        if (item instanceof IoTDomain) {
-            icon.setSrc("images/root-icon.png");
-        } else if (item instanceof ArchitectureSolution) {
-            icon.setSrc("images/collapsible-icon.png");
-        } else if (item instanceof QualityRequirement) {
-            icon.setSrc("images/collapsible-icon.png");
-        } else if (item instanceof Technology) {
-            icon.setSrc("images/leaf-icon.png");
+        while (current != null) {
+            path.add(current);
+            current = getParentNode(current);
         }
+
+        Collections.reverse(path);
+        return path;
     }
 }
