0d87f2edc36f5795b88d3bd48592c7ad87aa43b5
Fernando <fernandonrs@cos.ufrj.br>
2024-06-19 21:32:10 -0300

    Changed to use database H2 and buid the tree and trying charts.

--- src/main/java/br/ufrj/cos/components/chart/ChartComponent.java
+++ src/main/java/br/ufrj/cos/components/chart/ChartComponent.java
@@ -1,36 +1,75 @@
 package br.ufrj.cos.components.chart;
 
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Base64;
+import java.util.HashMap;
 
+import com.vaadin.flow.component.html.Image;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartUtils;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PiePlot;
+import org.jfree.chart.util.SortOrder;
 import org.jfree.data.general.DefaultPieDataset;
 import org.jfree.data.general.PieDataset;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ChartComponent {
 
-    public static File createPieChart() throws IOException {
-        PieDataset dataset = createDataset();
+    /***
+     * Create the Pie chart
+     * @return Vaadin Image
+     * @throws IOException
+     */
+    public Image createPieChart(HashMap<String, Integer> dataSet) throws IOException {
+        PieDataset dataset = createDataset(dataSet);
         JFreeChart chart = createChart(dataset);
 
         File imageFile = File.createTempFile("pie_chart", ".png");
         ChartUtils.saveChartAsPNG(imageFile, chart, 800, 600);
 
-        return imageFile;
+        return this.convertToImage(imageFile);
     }
 
-    private static PieDataset createDataset() {
-        DefaultPieDataset dataset = new DefaultPieDataset();
-        dataset.setValue("Category A", 50);
-        dataset.setValue("Category B", 20);
-        dataset.setValue("Category C", 30);
-        return dataset;
+    /***
+     * get the chart return and convert to Vaadin image
+     * @param imageFile
+     * @return
+     * @throws IOException
+     */
+    private Image convertToImage(File imageFile) throws IOException {
+        byte[] imageBytes = Files.readAllBytes(
+                Paths.get(imageFile.toURI()));
+
+        // Convert to Base64
+        String base64Image = Base64.getEncoder().encodeToString(imageBytes);
+        String imageUrl = "data:image/png;base64," + base64Image;
+
+        // Create Vaadin Image component
+        Image image = new Image(imageUrl, "Example image");
+        image.setWidth("600px");
+        image.setHeight("400px");
+
+        return image;
     }
 
-    private static JFreeChart createChart(PieDataset dataset) {
+    private PieDataset createDataset(HashMap<String, Integer> dataSet) {
+        DefaultPieDataset chartDataset = new DefaultPieDataset();
+
+        dataSet.keySet().forEach(k -> {
+            chartDataset.setValue(k, dataSet.get(k));
+        });
+
+        chartDataset.sortByKeys(SortOrder.ASCENDING);
+        return chartDataset;
+    }
+
+    private JFreeChart createChart(PieDataset dataset) {
         JFreeChart chart = ChartFactory.createPieChart(
                 "Pie Chart Example",
                 dataset,
--- src/main/java/br/ufrj/cos/views/home/HomeView.java
+++ src/main/java/br/ufrj/cos/views/home/HomeView.java
@@ -1,20 +1,32 @@
 package br.ufrj.cos.views.home;
 
+import br.ufrj.cos.components.chart.ChartComponent;
 import br.ufrj.cos.views.BaseView;
 import br.ufrj.cos.views.MainLayout;
+import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
 import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Base64;
 
 @PageTitle("Arch IoT - Home")
 @Route(value = "", layout = MainLayout.class)
 @RouteAlias(value = "", layout = MainLayout.class)
 public class HomeView extends BaseView {
 
-    public HomeView() {
+    @Autowired
+    ChartComponent chart;
+
+    public HomeView() throws IOException {
         VerticalLayout layoutColumn2 = new VerticalLayout();
         HorizontalLayout layoutRow = new HorizontalLayout();
         //header
@@ -26,8 +38,6 @@ public class HomeView extends BaseView {
         layoutRow.setWidth("100%");
         layoutRow.setHeight("min-content");
 
-        getContent().add(layoutColumn2);
-        getContent().add(layoutRow);
-        layoutRow.add(this.createHeader("Heading"));
+        layoutRow.add(chart.createPieChart());
     }
 }
