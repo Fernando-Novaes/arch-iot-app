29385bd6b29c19e4e73dcea0d21e48ddaa4b2a42
fernando <fernandonr@cos.ufrj.br>
2024-06-21 23:26:08 -0300

    Chart running and record DataSet crated.

---
+++ src/main/java/br/ufrj/cos/components/chart/ChartData.java
@@ -0,0 +1,15 @@
+package br.ufrj.cos.components.chart;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class ChartData {
+
+    private String description;
+    private Integer value;
+
+}
--- src/main/java/br/ufrj/cos/views/home/HomeView.java
+++ src/main/java/br/ufrj/cos/views/home/HomeView.java
@@ -13,12 +13,7 @@ import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Base64;
-import java.util.HashMap;
 
 @Component
 @PageTitle("Arch IoT - Home")
